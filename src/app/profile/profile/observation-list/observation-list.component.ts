347a22fa751e61b24f09a314d3b2cb1405694585
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2020-06-14 20:36:49 +0200

    fix race condition when loading profile page

--- src/app/profile/profile/observation-list/observation-list.component.ts
+++ src/app/profile/profile/observation-list/observation-list.component.ts
@@ -13,7 +13,7 @@ import { first, map, mergeAll } from 'rxjs/operators';
   styleUrls: ['./observation-list.component.scss']
 })
 export class ObservationListComponent implements OnInit, OnDestroy {
-  @Input() _userId: string;
+  @Input() userId: string;
 
   latestIndividualObservations: Observable<IndividualPhenophase[]>;
   limitIndividuals = new BehaviorSubject<number>(4);
@@ -26,60 +26,48 @@ export class ObservationListComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
-  }
+    // combine the list of individuals with their phenophase
+    this.latestIndividualObservations = combineLatest(
+      [this.limitIndividuals, this.individualService.listByUser(this.userId)],
+      (limit, individuals) =>
+        combineLatest(
+          individuals
+            .sort((l, r) => {
+              const l_hasnt_last_obs = l.last_observation_date ? false : true;
+              const r_hasnt_last_obs = r.last_observation_date ? false : true;
 
-  get userId() {
-    return this._userId;
-  }
-
-  @Input()
-  set userId(userId: string) {
-    this._userId = userId;
-
-    if (userId) { // userId is undefined at first on own profile
-      // combine the list of individuals with their phenophase
-      this.latestIndividualObservations = combineLatest(
-        [this.limitIndividuals, this.individualService.listByUser(this.userId)],
-        (limit, individuals) =>
-          combineLatest(
-            individuals
-              .sort((l, r) => {
-                const l_hasnt_last_obs = l.last_observation_date ? false : true;
-                const r_hasnt_last_obs = r.last_observation_date ? false : true;
-
-                if (l_hasnt_last_obs && r_hasnt_last_obs) {
-                  return 0;
+              if (l_hasnt_last_obs && r_hasnt_last_obs) {
+                return 0;
+              }
+              if (l_hasnt_last_obs) {
+                return -1;
+              }
+              if (r_hasnt_last_obs) {
+                return 1;
+              } else {
+                return (r.last_observation_date as any).toMillis() - (l.last_observation_date as any).toMillis();
+              }
+            })
+            .slice(0, limit)
+            .map(individual => {
+              return combineLatest(
+                this.masterdataService.getSpeciesValue(individual.species),
+                this.masterdataService.getPhenophaseValue(individual.species, individual.last_phenophase),
+                (species, phenophase) => {
+                  return {
+                    individual: individual,
+                    species: species,
+                    lastPhenophase: phenophase,
+                    imgUrl: this.individualService.getImageUrl(individual, true).pipe(
+                      first(),
+                      map(u => (u === null ? 'assets/img/pic_placeholder.svg' : u))
+                    )
+                  } as IndividualPhenophase;
                 }
-                if (l_hasnt_last_obs) {
-                  return -1;
-                }
-                if (r_hasnt_last_obs) {
-                  return 1;
-                } else {
-                  return (r.last_observation_date as any).toMillis() - (l.last_observation_date as any).toMillis();
-                }
-              })
-              .slice(0, limit)
-              .map(individual => {
-                return combineLatest(
-                  this.masterdataService.getSpeciesValue(individual.species),
-                  this.masterdataService.getPhenophaseValue(individual.species, individual.last_phenophase),
-                  (species, phenophase) => {
-                    return {
-                      individual: individual,
-                      species: species,
-                      lastPhenophase: phenophase,
-                      imgUrl: this.individualService.getImageUrl(individual, true).pipe(
-                        first(),
-                        map(u => (u === null ? 'assets/img/pic_placeholder.svg' : u))
-                      )
-                    } as IndividualPhenophase;
-                  }
-                );
-              })
-          )
-      ).pipe(mergeAll());
-          }
+              );
+            })
+        )
+    ).pipe(mergeAll());
   }
 
   isOwner(): boolean {
