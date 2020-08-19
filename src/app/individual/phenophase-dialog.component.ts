bf26d556b5396354065dfc91c9a0c8c96d4bb8df
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2020-08-19 20:04:43 +0200

    fix observation datepicker for small devices #43

--- src/app/individual/phenophase-dialog.component.ts
+++ src/app/individual/phenophase-dialog.component.ts
@@ -1,15 +1,23 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { PhenophaseObservation } from '../observation/phenophase-observation';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'phenophase-dialog',
   templateUrl: 'phenophase-dialog.component.html'
 })
 export class PhenophaseDialogComponent {
+  showTouchCalendar: Observable<boolean> = this.breakpointObserver
+    .observe('(max-height: 700px)')
+    .pipe(map(result => result.matches));
+
   constructor(
     public dialogRef: MatDialogRef<PhenophaseDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: PhenophaseObservation
+    @Inject(MAT_DIALOG_DATA) public data: PhenophaseObservation,
+    private breakpointObserver: BreakpointObserver
   ) {}
 
   close(): void {
