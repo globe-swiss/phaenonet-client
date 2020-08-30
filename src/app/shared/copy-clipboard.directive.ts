6e405c1ddb09cb7cce544b335198565d32d4e6bb
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2020-08-30 16:58:03 +0200

    fix profile clipboard copy for iphone/safari #46

--- src/app/shared/copy-clipboard.directive.ts
+++ src/app/shared/copy-clipboard.directive.ts
@@ -5,24 +5,36 @@ export class CopyClipboardDirective {
   @Input('app-copy-clipboard')
   public payload: string;
 
-  @Output('copied')
+  @Output()
   public copied: EventEmitter<string> = new EventEmitter<string>();
 
   @HostListener('click', ['$event'])
   public onClick(event: MouseEvent): void {
     event.preventDefault();
-    if (!this.payload) return;
 
-    let listener = (e: ClipboardEvent) => {
-      let clipboard = e.clipboardData || window['clipboardData'];
-      clipboard.setData('text', this.payload.toString());
-      e.preventDefault();
+    if (!this.payload) { return; }
+    this.copyClipboard(this.payload);
+    this.copied.emit(this.payload);
+  }
 
-      this.copied.emit(this.payload);
-    };
+  // workaround for safari/iphone https://bugs.webkit.org/show_bug.cgi?id=156529
+  private copyClipboard(text: string) {
+    const textArea = document.createElement('input') as HTMLInputElement;
 
-    document.addEventListener('copy', listener, false);
-    document.execCommand('copy');
-    document.removeEventListener('copy', listener, false);
+    textArea.value = text;
+    document.body.appendChild(textArea);
+
+    if (navigator.userAgent.match(/ipad|iphone/i)) {
+      const range = document.createRange();
+      range.selectNodeContents(textArea);
+      const selection = window.getSelection();
+      selection.removeAllRanges();
+      selection.addRange(range);
+      textArea.setSelectionRange(0, 999999);
+  } else {
+      textArea.select();
+  }
+  document.execCommand('copy');
+  document.body.removeChild(textArea);
   }
 }
