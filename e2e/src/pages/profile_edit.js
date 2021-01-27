bad0c30d2fdbace6520106653b14134bf78e56fc
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2021-01-27 21:47:01 +0100

    fix profile edit bug #109 (#112)
    * fix auth getUser function to return correct data
    * add e2e test for nickname validation
    * upgrade playwright to 1.8

--- e2e/src/pages/profile_edit.js
+++ e2e/src/pages/profile_edit.js
@@ -1,5 +1,15 @@
 module.exports = {
   url(user) {
     return `/profile/${user.id}/edit`;
-  }
+  },
+  fields: {
+    nickname: { css: 'input#nickname' },
+    firstname: { css: 'input#firstname' },
+    lastname: { css: 'input#lastname' }
+  },
+  editPasswordIcon: { css: 'mat-icon edit' },
+  saveButton: { css: 'button#save-button' },
+  cancelButton: { css: 'button#cancel-button' },
+  saveButtonEnabled: { css: 'button#save-button:not(:disabled)' },
+  saveButtonDisabled: { css: 'button#save-button:disabled' }
 };
