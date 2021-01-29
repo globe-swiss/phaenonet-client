b542c84bb5a345216e0927ab2e09fe38d0cd2452
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2021-01-29 09:10:13 +0000

    report missing translations to sentry.io

---
+++ src/app/shared/SentryMissingTranslationHandler.ts
@@ -0,0 +1,18 @@
+import { MissingTranslationHandler, MissingTranslationHandlerParams } from '@ngx-translate/core';
+import * as Sentry from '@sentry/angular';
+import { environment } from 'src/environments/environment';
+
+export class SentryMissingTranslationHandler implements MissingTranslationHandler {
+  handle(params: MissingTranslationHandlerParams) {
+    if (params.translateService.currentLang !== 'de-CH' && !Number(params.key)) {
+      if (environment.name === 'local') {
+        console.log(this.getMsg(params));
+      }
+      Sentry.captureMessage(this.getMsg(params));
+    }
+  }
+
+  getMsg(params: MissingTranslationHandlerParams) {
+    return 'Translation Error: ' + params.key + ' not found for ' + params.translateService.currentLang;
+  }
+}
