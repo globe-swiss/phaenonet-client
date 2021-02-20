8265d847a5f03acec990f6d759178f5de3efba25
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2021-02-20 14:40:39 +0100

    fix altitude change detection #135 (#136)

--- src/app/individual/individual-edit/individual-edit-header/geopos.service.ts
+++ src/app/individual/individual-edit/individual-edit-header/geopos.service.ts
@@ -4,21 +4,24 @@ import { BehaviorSubject } from 'rxjs';
 @Injectable()
 export class GeoposService {
   readonly initialGeopos: google.maps.LatLngLiteral = { lat: 46.818188, lng: 8.227512 };
-  center$: BehaviorSubject<google.maps.LatLngLiteral> = new BehaviorSubject(this.initialGeopos);
-  geopos$: BehaviorSubject<google.maps.LatLngLiteral> = new BehaviorSubject(this.initialGeopos);
-  altitude$: BehaviorSubject<number> = new BehaviorSubject(0);
+  private center: BehaviorSubject<google.maps.LatLngLiteral> = new BehaviorSubject(this.initialGeopos);
+  private geopos: BehaviorSubject<google.maps.LatLngLiteral> = new BehaviorSubject(this.initialGeopos);
+  private altitude: BehaviorSubject<number> = new BehaviorSubject(0);
+  center$ = this.center.asObservable();
+  geopos$ = this.geopos.asObservable();
+  altitude$ = this.altitude.asObservable();
 
   elevator = new google.maps.ElevationService();
 
   constructor() {}
 
-  public init() {
+  init() {
     this.update(new google.maps.LatLng(this.initialGeopos));
   }
 
-  public update(latLng: google.maps.LatLng) {
-    this.geopos$.next(latLng.toJSON());
-    this.center$.next(latLng.toJSON());
+  update(latLng: google.maps.LatLng) {
+    this.geopos.next(latLng.toJSON());
+    this.center.next(latLng.toJSON());
     this.updateAltitude(latLng);
   }
 
@@ -26,9 +29,17 @@ export class GeoposService {
     this.elevator.getElevationForLocations({ locations: [latLng] }, (results, status) => {
       if (status === 'OK') {
         if (results[0]) {
-          this.altitude$.next(results[0].elevation);
+          this.altitude.next(Math.round(results[0].elevation));
         }
       }
     });
+  }
+
+  getAltitude() {
+    return this.altitude.value;
+  }
+
+  getGeoPos() {
+    return this.geopos.value;
   }
 }
