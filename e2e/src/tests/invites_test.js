d2a313eebf9be3f941d6d785bd39ba9c5c48c1a0
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2021-10-29 09:54:46 +0000

    adapt e2e tests

--- e2e/src/tests/invites_test.js
+++ e2e/src/tests/invites_test.js
@@ -1,16 +1,20 @@
 Feature('Invites');
 
-Scenario('test component present invites', ({ I, invitesPage }) => {
+Before(async ({ I, invitesPage }) => {
+  await I.clearTestData();
   I.login();
   I.visit(invitesPage.url);
+});
+
+Scenario('test component present invites', ({ I, invitesPage }) => {
   I.checkElementsPresent(invitesPage.components);
 });
 
 Scenario('test component send invite', ({ I, invitesPage }) => {
-  I.login();
-  I.visit(invitesPage.url);
+  I.see('keine Einladungen vorhanden');
   I.click(invitesPage.invitesList.inviteButton);
   I.fillField(invitesPage.invitesDialog.textfield, 'aaazzz@example.com');
   I.click(invitesPage.invitesDialog.sendButton);
   I.see('aaazzz@example.com', invitesPage.invitesList.listItems);
+  I.seeElement(invitesPage.invitesList.inviteButton);
 });
