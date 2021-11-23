376c9927e688f53ef654c77568d3e4d3fd03f2df
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2021-11-23 18:48:44 +0000

    fix eslint findings

--- src/app/shared/HttpErrorHandler.ts
+++ src/app/shared/HttpErrorHandler.ts
@@ -5,10 +5,10 @@ import { Observable, ObservableInput, of, throwError } from 'rxjs';
 export class HttpErrorHandler {
   constructor(protected router: Router) {}
 
-  // override in subclass if you want a custom error handling
-  detailLoadingFailed<T>(error: any, caught: Observable<T>): ObservableInput<any> {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  detailLoadingFailed<T>(error: unknown, caught: Observable<T>): ObservableInput<unknown> {
     if (error instanceof HttpErrorResponse && error.status == 404) {
-      this.router.navigate(['/404'], { skipLocationChange: true });
+      void this.router.navigate(['/404'], { skipLocationChange: true });
       return of();
     }
     return throwError(error);
