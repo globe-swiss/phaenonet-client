8902c62376a95679c75f9d0bca25264f0c536fa9
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2022-01-09 13:46:57 +0000

    codacy review findings

--- src/app/individual/individual.service.ts
+++ src/app/individual/individual.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
-import internal from 'events';
 import { combineLatest, from, Observable, of } from 'rxjs';
 import { first, map, mergeAll } from 'rxjs/operators';
 import { AuthService } from '../auth/auth.service';
