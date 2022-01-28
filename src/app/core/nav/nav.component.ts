24852da7706c584df16abc9df771fab962721d54
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2022-01-28 15:17:23 +0000

    link logo to phaenonet website

--- src/app/core/nav/nav.component.ts
+++ src/app/core/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
+import { LanguageService } from '../language.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +8,13 @@ import { AuthService } from '../../auth/auth.service';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private languageService: LanguageService) {}
 
   isLoggedIn(): boolean {
     return this.authService.isLoggedIn();
+  }
+
+  lang(): string {
+    return this.languageService.determineCurrentLang().substring(0, 2);
   }
 }
