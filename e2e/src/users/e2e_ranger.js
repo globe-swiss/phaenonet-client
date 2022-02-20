2dc4ce3b8b123b3e7271043f5097693687fcd574
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2022-02-20 17:46:00 +0000

    fix: roles observable update on login/logout

---
+++ e2e/src/users/e2e_ranger.js
@@ -0,0 +1,9 @@
+module.exports = {
+  nickname: 'e2e-ranger-nick',
+  name: 'e2e-ranger-name',
+  surname: 'e2e-ranger-surname',
+  email: 'e2e-ranger@app.phaenonet.ch',
+  language: 'Deutsch',
+  password: '1234567',
+  id: 'JIcn8kFpI4fYYcbdi9QzPlrHomn1'
+};
