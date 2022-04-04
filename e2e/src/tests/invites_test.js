60e3b7fcd533739f2617bf466fa4232fbe854780
Fabian Page <fabian.page@tegonal.com>
2022-04-04 09:47:50 +0200

    fix e2e

--- e2e/src/tests/invites_test.js
+++ e2e/src/tests/invites_test.js
@@ -13,8 +13,11 @@ Scenario('test component present invites', ({ I, invitesPage }) => {
 Scenario('test component send invite', ({ I, invitesPage }) => {
   I.see('keine Einladungen vorhanden');
   I.click(invitesPage.invitesList.inviteButton);
+  I.wait(1);
+  // I.waitForText('PERSON EINLADEN');
   I.fillField(invitesPage.invitesDialog.textfield, 'verylongunusedemailadress@example.com');
+  // I.waitForText('verylongunusedemailadress@example.com');
   I.click(invitesPage.invitesDialog.sendButton);
-  I.retry(10).see('verylongunusedemailadress@example.com', invitesPage.invitesList.openInviteList);
+  I.see('verylongunusedemailadress@example.com', invitesPage.invitesList.openInviteList);
   I.seeElement(invitesPage.invitesList.inviteButton);
 });
