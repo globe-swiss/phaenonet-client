3a45f288e330e24d2560d13b5f72bd1f519e4ca3
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2022-08-26 09:21:43 +0000

    add unit tests functionality
    * add jest to project
    * update eslint rules
    * integrate with github

---
+++ src/app/shared/local.service.spec.ts
@@ -0,0 +1,87 @@
+import { LocalService } from './local.service';
+
+describe('Service: Local', () => {
+  const KEY = 'test_key';
+  const VALUE = 'test_value';
+  let fixture: LocalService;
+
+  beforeEach(() => {
+    fixture = new LocalService();
+  });
+
+  it('should create', () => {
+    expect(fixture).toBeTruthy();
+  });
+
+  describe('localStorageSet', () => {
+    it('should handle write storage when unavailable', () => {
+      jest.spyOn(console, 'error').mockReturnValue();
+      jest.spyOn(fixture, 'storageAvailable').mockReturnValue(false);
+      const localStorageSpy = jest.spyOn(localStorage, 'setItem');
+
+      const result = fixture.localStorageSet(KEY, VALUE);
+
+      expect(result).toBeFalsy();
+      expect(localStorageSpy).not.toHaveBeenCalled();
+    });
+
+    it('should handle write on storage available', () => {
+      jest.spyOn(fixture, 'storageAvailable').mockReturnValue(true);
+      const localStorageSpy = jest.spyOn(localStorage, 'setItem');
+
+      const result = fixture.localStorageSet(KEY, VALUE);
+
+      expect(result).toBeTruthy();
+      expect(localStorageSpy).toHaveBeenCalledWith(KEY, VALUE);
+    });
+  });
+
+  describe('localStorageGet', () => {
+    it('should handle read storage unavailable', () => {
+      jest.spyOn(fixture, 'storageAvailable').mockReturnValue(false);
+      const localStorageSpy = jest.spyOn(localStorage, 'getItem');
+
+      const result = fixture.localStorageGet(KEY);
+
+      expect(result).toBeUndefined();
+      expect(localStorageSpy).not.toHaveBeenCalled();
+    });
+
+    it('should handle read on storage available', () => {
+      jest.spyOn(fixture, 'storageAvailable').mockReturnValue(true);
+      const localStorageSpy = jest.spyOn(localStorage, 'getItem');
+      localStorage.setItem(KEY, VALUE);
+
+      const result = fixture.localStorageGet(KEY);
+
+      expect(result).toEqual(VALUE);
+      expect(localStorageSpy).toHaveBeenCalledWith(KEY);
+    });
+  });
+
+  describe('storageAvailable', () => {
+    it.skip('should handle localStorage unavailabe', () => {
+      window.localStorage = undefined; // how to set localstore to undefined?
+
+      const result = fixture.storageAvailable('localStorage');
+
+      expect(result).toEqual(false);
+    });
+
+    it('should handle localStorage write error', () => {
+      jest.spyOn(localStorage, 'setItem').mockImplementation(() => {
+        throw new Error('cannot write');
+      });
+
+      const result = fixture.storageAvailable('localStorage');
+
+      expect(result).toEqual(false);
+    });
+
+    it('should detect that storage is avaiable', () => {
+      const result = fixture.storageAvailable('localStorage');
+
+      expect(result).toEqual(true);
+    });
+  });
+});
