4a6c1b177d440c20d068c8a8a2dd5f742ae657e7
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2022-11-21 08:22:57 +0100

    tag sentry errors (#393)

--- src/app/shared/GlobalErrorHandler.ts
+++ src/app/shared/GlobalErrorHandler.ts
@@ -1,8 +1,13 @@
 import { ErrorHandler, Injectable } from '@angular/core';
 import * as Sentry from '@sentry/angular';
+import { firstValueFrom } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+import { UserService } from '../profile/user.service';
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
+  constructor(private authService: AuthService, private userService: UserService) {}
+
   private sentryErrorHandler = Sentry.createErrorHandler({ showDialog: false });
 
   handleError(error: ErrorEvent): void {
@@ -12,7 +17,17 @@ export class GlobalErrorHandler implements ErrorHandler {
       Sentry.captureMessage('Chunk load failed. Reloading.');
       window.location.reload();
     } else {
-      this.sentryErrorHandler.handleError(error);
+      const userid = this.authService.getUserId();
+      if (userid) {
+        void firstValueFrom(this.userService.getSource()).then(sourceType => {
+          Sentry.setUser({ id: userid });
+          Sentry.setTag('source', sourceType);
+          this.sentryErrorHandler.handleError(error);
+        });
+      } else {
+        Sentry.setTag('source', 'none');
+        this.sentryErrorHandler.handleError(error);
+      }
     }
   }
 }
