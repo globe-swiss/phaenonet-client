950a4755c7f49a0fad6d2dd725ffc12e6757d466
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2023-02-14 17:00:03 +0000

    fix flaky station tests

--- e2e/src/tests/station_test.js
+++ e2e/src/tests/station_test.js
@@ -6,6 +6,7 @@ Before(({ I }) => {
 
 Scenario('test meteoswiss station', async ({ I, stationsPage }) => {
   I.visit(stationsPage, stationsPage.url('2018_ENB'));
+  I.waitForText('Winterlinde'); // wait data is loaded
   await I.checkVisual('stations_view-meteoswiss');
 })
   .tag('@visual')
@@ -13,6 +14,7 @@ Scenario('test meteoswiss station', async ({ I, stationsPage }) => {
 
 Scenario('test wld station', async ({ I, stationsPage }) => {
   I.visit(stationsPage, stationsPage.url('2018_wld_380222'));
+  I.waitForText('Rotbuche 10'); // wait data is loaded
   await I.checkVisual('stations_view-wld');
 })
   .tag('@visual')
