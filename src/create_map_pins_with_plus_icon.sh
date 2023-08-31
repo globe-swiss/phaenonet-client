58ef088192e0ed52ff2be6c081e80aa426769d7e
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2023-08-31 16:02:17 +0000

    mast config & icons

--- src/create_map_pins_with_plus_icon.sh
+++ src/create_map_pins_with_plus_icon.sh
@@ -4,9 +4,9 @@ set -e
 current_dir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd "$current_dir/assets/img/map_pins"
 
-# mkdir assets/img/map_pins/de-CH/generated
-# mkdir assets/img/map_pins/fr-CH/generated
-# mkdir assets/img/map_pins/it-CH/generated
+mkdir -p de-CH/generated
+mkdir -p fr-CH/generated
+mkdir -p it-CH/generated
 
 generate_images() {
   for f in *.png; do
@@ -26,6 +26,3 @@ cd ".."
 cd "it-CH"
 generate_images
 cd ".."
-
-
-
