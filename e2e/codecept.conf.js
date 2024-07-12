69e7bb14845f0b7fe5d7f37be8ddd93a8e58320e
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2024-07-12 11:14:06 +0000

    add e2e test coverage

--- e2e/codecept.conf.js
+++ e2e/codecept.conf.js
@@ -71,6 +71,16 @@ exports.config = {
     },
     subtitles: {
       enabled: true
+    },
+    coverage: {
+      enabled: true,
+      debug: true,
+      name: 'E2E-Test Coverage Report',
+      outputDir: 'output/coverage-reports',
+      reports: ['codecov', 'console-summary', 'v8'],
+      sourceFilter: { '**/src/**': true, '**/*.js': false },
+      entryFilter: { '**/main.js': true },
+      cleanCache: true
     }
   }
 };
