434c26bb8b3bc8614b6f7e52a591f57dc33b558b
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2024-09-01 13:31:16 +0000

    fix id collision

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  selector: 'app-login-component-dummy',
   template: '<router-outlet></router-outlet>'
 })
 export class LoginComponent {}
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  selector: 'app-map-component-dummy',
   template: '<router-outlet></router-outlet>'
 })
 export class MapComponent {}
