4bc8674a647ebf29a8cf464d1fed1878d735797c
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2025-05-16 12:27:52 +0000

    use google map api loader

--- src/app/core/services/google-maps-loader.service.ts
+++ src/app/core/services/google-maps-loader.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
 import { environment } from '~/environments/environment';
 
 @Injectable({
@@ -14,33 +15,21 @@ export class GoogleMapsLoaderService {
     }
 
     if (this.loadPromise === null) {
-      this.loadPromise = new Promise((resolve, reject) => {
-        const script = document.createElement('script');
-        script.src = `https://maps.googleapis.com/maps/api/js?key=${environment.firebaseConfig.apiKey}`;
-        script.async = true;
-        script.onload = async () => {
-          try {
-            await this.initLibraries();
-            this.apiLoaded = true;
+      const loader = new Loader({
+        apiKey: environment.firebaseConfig.apiKey,
+        version: 'quarterly'
+      });
 
-            resolve();
-          } catch (error) {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-            reject(new Error('Google Maps API failed to load: ' + (error?.message || error)));
-          }
-        };
-        script.onerror = () => reject(new Error('Google Maps API failed to load.'));
-        document.head.appendChild(script);
+      this.loadPromise = Promise.all([
+        loader.importLibrary('core'),
+        loader.importLibrary('maps'),
+        loader.importLibrary('marker'),
+        loader.importLibrary('elevation')
+      ]).then(() => {
+        this.apiLoaded = true;
       });
     }
 
     return this.loadPromise;
-  }
-
-  private async initLibraries(): Promise<void> {
-    await google.maps.importLibrary('core');
-    await google.maps.importLibrary('maps');
-    await google.maps.importLibrary('marker');
-    await google.maps.importLibrary('elevation');
   }
 }
